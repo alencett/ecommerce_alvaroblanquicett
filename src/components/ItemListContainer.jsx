93d3829cbed687e9ad6ebc7e7a81ccf8e077a73e
Alvaro Blanquicett <alvaroblanquicett@gmail.com>
2023-03-29 21:14:06 -0500

    agregado componentes ItemCount y ItemListContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import Title from "./Title";
+import ItemCount from "./ItemCount";
 
-export const ItemListContainer = () => {
+export const ItemListContainer = ({ texto}) => {
+
+    const onAdd = (quantity) => {
+        if(quantity == 1) { 
+            console.log(`Compraste ${quantity} unidad!`)}
+        else {
+            console.log(`Compraste ${quantity} unidades!`)  
+        }
+    }
+
     return (
-        < Title greeting="(nombre de usuario)"/>
+        <>
+            < Title greeting={texto}/>
+            < ItemCount initial={1} stock={5} onAdd={onAdd}/>
+        </>
     );
 }
 
